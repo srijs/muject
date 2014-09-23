d1a32c72b32507e79f2cb83bfbdaa2ace17b0104
Sam Reis <sreis@atlassian.com>
2014-09-24 00:59:11 +1000

    tune size

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 module.exports = function (require) {
 
-  return function inject (name, deps) {
+  return function inject (module, deps) {
 
-    var exports = {}, module = typeof name === 'function' ? name : require(name);
+    var exports = {};
 
-    if (!(deps = deps || module.inject))
-      return module;
+    try { module = require(module); } finally {
 
-    return module.apply(exports, deps.map(function (dep) {
-      return inject[dep];
-    })) || exports;
+      return (deps = deps || module.inject) ?
+      module.apply(exports, deps.map(function (dep) {
+        return inject[dep];
+      })) || exports : module;
+
+    }
 
   };
 
