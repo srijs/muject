2d61487f6b165145a801ab9b25a9e3dd1210e349
Sam Reis <sreis@atlassian.com>
2014-09-24 10:42:47 +1000

    reduce size

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (require) {
 
   return function inject (module, deps) {
 
-    if (!module.apply) module = require(module);
+    module = module.apply ? module : require(module);
 
     return (deps = deps || module.inject) ?
     module.apply(module = {}, deps.map(function (dep) {
